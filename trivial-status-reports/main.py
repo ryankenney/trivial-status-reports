1efbd05650ef3422c403ec5aab4971c97dd9363c
Ryan Kenney <ryan@ryankenney.info>
2020-04-25 12:23:59 -0700

    * Added support for a top level config file * Added code docs

--- trivial-status-reports/main.py
+++ trivial-status-reports/main.py
@@ -6,17 +6,42 @@ from pathlib import Path
 import os
 import glob
 
-
-print (datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d_%H-%M-%S_%Z'))
-print (datetime.now(timezone('utc')).strftime('%Y-%m-%d_%H-%M-%S_%Z'))
-print (datetime.now(timezone('US/Eastern')).timestamp())
-print (datetime.now(timezone('utc')).timestamp())
-print (datetime.now().timestamp())
-
 _unsafe_filesystem_chars = re.compile(r'[^0-9a-zA-Z_\-]+')
-_multi_dot_pattern = re.compile(r'\.+')
+
+# Public Methods
+# ----------------
+
+def define_config(base_directory,
+	config_timezone=None,
+	config_summary_title=None,
+	config_overview_section_md=None):
+	"""
+	Writes the top-level config file (config.json) for the target directory.
+
+	:param base_directory: The base directory where the config and and all output should go.
+	:param config_summary_title: The title to apply to the summary page.
+	:param config_overview_section_md: An optional section to include at the top of the markdown summary page.
+	"""
+	base_directory = Path(base_directory)
+	config = _load_config(base_directory)
+	if config_timezone is not None:
+		config['timezone'] = config_timezone
+	if config_summary_title is not None:
+		config['summary_title'] = config_summary_title
+	if config_overview_section_md is not None:
+		config['overview_section_md'] = config_overview_section_md
+	_write_config(base_directory, config)
 
 def define_test_definition(base_directory, test_id, title, description, timeout_secs):
+	"""
+	Updates/creates a test definition to the target directory. This only updates the json metadata for the test definition (not markdown).
+
+	:param base_directory: The base directory where the config and and all output should go.
+	:param test_id: A UID for the test definition. Legal chars include `[0-9a-zA-Z_-]`.
+	:param title: A human-readable title.
+	:param description: A detailed description. Double-linebreaks will be treated as paragraph breaks in markdown.
+	:param timeout_secs: (Feature TBD.) The number of seconds with no reports before the current state of the test is set to `TIMEOUT`.
+	"""
 	if type(timeout_secs) != int:
 		raise Exception('Invalid timeout_secs value. Must be an integer.')
 	test_id = _sanitize_filesystem_path(test_id)
@@ -32,6 +57,14 @@ def define_test_definition(base_directory, test_id, title, description, timeout_
 		json.dump(test_definition, file, indent=1)
 
 def report_test_run(base_directory, test_id, state, log):
+	"""
+	Updates/creates a test run log to the target directory. This only updates the json metadata for the run (not markdown).
+
+	:param base_directory: The base directory where the config and and all output should go.
+	:param test_id: A UID for the test definition. Legal chars include `[0-9a-zA-Z_-]`.
+	:param state: Short string used to indicate status. `OK` and `FAILED` are commonly used.
+	:param log: A detailed log of the test run, which will be renderd as a code-block in markdown.
+	"""
 	test_id = _sanitize_filesystem_path(test_id)
 	# Use UTC timestamp as run_id
 	run_id = datetime.now().timestamp()
@@ -46,12 +79,40 @@ def report_test_run(base_directory, test_id, state, log):
 	with open(target_dir / (str(run_id)+'.json'), 'w') as file:
 		json.dump(test_run, file, indent=1)
 
-def generate_markdown(base_directory, tz):
-	_generate_test_run_markdown(base_directory, tz)
-	_generate_test_summary_markdown(base_directory, tz)
-	_generate_main_summary_markdown(base_directory, tz)
+def generate_markdown(base_directory):
+	"""
+	Updates all markdown documents from the json metadata files.
 
-def _generate_test_run_markdown(base_directory, tz):
+	:param base_directory: The base directory where the config and and all output should go.
+	"""
+	config = _load_config(base_directory)
+	_generate_test_run_markdown(base_directory, config)
+	_generate_test_summary_markdown(base_directory, config)
+	_generate_main_summary_markdown(base_directory, config)
+
+# Internal Methods
+# ----------------
+
+def _load_config(base_directory):
+	config_file = base_directory / 'config.json'
+	if not config_file.exists():
+		return _get_default_config()
+	with open(config_file, 'r') as file:
+		return json.load(file)
+
+def _write_config(base_directory, config):
+	base_directory.mkdir(parents=True, exist_ok=True)
+	with open(base_directory / 'config.json', 'w') as file:
+		json.dump(config, file, indent=1)
+
+def _get_default_config():
+	return {
+		'timezone': 'US/Eastern',
+		'summary_title': 'Status Summary',
+		'overview_section_md': None
+	}
+
+def _generate_test_run_markdown(base_directory, config):
 	tests_dir = Path(base_directory) / 'tests'
 	tests_dir.mkdir(parents=True, exist_ok=True)
 	markdown_tests_dir = Path(base_directory) / 'md/tests'
@@ -72,7 +133,7 @@ def _generate_test_run_markdown(base_directory, tz):
 				run = json.load(file)
 			test_id = run['test_id']
 			run_id = run["run_id"]
-			run_time = datetime.fromtimestamp(run['run_id'], tz).strftime('%Y-%m-%d %H-%M-%S %Z')
+			run_time = datetime.fromtimestamp(run['run_id'], timezone(config['timezone'])).strftime('%Y-%m-%d %H-%M-%S %Z')
 			run_state = run["state"]
 			markdown_lines = []
 			markdown_lines.append(f'Test [{test_id}] Run [{run_id}]')
@@ -94,7 +155,7 @@ def _generate_test_run_markdown(base_directory, tz):
 			with open(markdown_test_dir / (str(run_id)+'.md'), 'w') as file:
 				file.write('\n'.join(markdown_lines))
 
-def _generate_test_summary_markdown(base_directory, tz):
+def _generate_test_summary_markdown(base_directory, config):
 	tests_dir = Path(base_directory) / 'tests'
 	tests_dir.mkdir(parents=True, exist_ok=True)
 	markdown_tests_dir = Path(base_directory) / 'md/tests'
@@ -137,7 +198,7 @@ def _generate_test_summary_markdown(base_directory, tz):
 			test_id = run['test_id']
 			run_id = run['run_id']
 			# TODO: De-duplicate call
-			run_time = datetime.fromtimestamp(run['run_id'], tz).strftime('%Y-%m-%d %H-%M-%S %Z')
+			run_time = datetime.fromtimestamp(run['run_id'], timezone(config['timezone'])).strftime('%Y-%m-%d %H-%M-%S %Z')
 			run_state = run['state']
 			markdown_lines.append(f'* [{run_time}]({test_id}/{run_id}.md) [**{run_state}**]')
 
@@ -146,16 +207,20 @@ def _generate_test_summary_markdown(base_directory, tz):
 		with open(markdown_tests_dir / (str(test_id)+'.md'), 'w') as file:
 			file.write('\n'.join(markdown_lines))
 
-def _generate_main_summary_markdown(base_directory, tz):
+def _generate_main_summary_markdown(base_directory, config):
 	tests_dir = Path(base_directory) / 'tests'
 	tests_dir.mkdir(parents=True, exist_ok=True)
 
 	# TODO: Support title/description config files
 
 	markdown_lines = []
-	markdown_lines.append('System Summary')
+	markdown_lines.append(config['summary_title'])
 	markdown_lines.append('========')
 	markdown_lines.append('')
+
+	if 'overview_section_md' in config:
+		markdown_lines.append(config['overview_section_md'])
+		markdown_lines.append('')
 
 	test_defs = []
 	for test_file in tests_dir.glob('*.json'):
@@ -186,7 +251,7 @@ def _generate_main_summary_markdown(base_directory, tz):
 		run_id = run['run_id']
 		run_state = run['state']
 		# TODO: De-duplicate call
-		run_time = datetime.fromtimestamp(run['run_id'], tz).strftime('%Y-%m-%d %H-%M-%S %Z')
+		run_time = datetime.fromtimestamp(run['run_id'], timezone(config['timezone'])).strftime('%Y-%m-%d %H-%M-%S %Z')
 
 		markdown_lines.append(f'* [{run_state}](md/tests/{test_id}/{run_id}.md) [{test_title}](md/tests/{test_id}.md) ({run_time})')
 
@@ -195,76 +260,12 @@ def _generate_main_summary_markdown(base_directory, tz):
 	with open(base_directory / 'summary.md', 'w') as file:
 		file.write('\n'.join(markdown_lines))
 
-def write_update_to_markdown(test_results, base_directory, tz=timezone('US/Eastern')):
-	base_directory = Path(base_directory)
-	runs_directory = Path(base_directory) / 'runs'
-
-	# Write run metadata to json
-	run_id = datetime.now(tz).strftime('%Y-%m-%d_%H-%M-%S_%Z')
-	_validated_and_sanitize_test_results_json(test_results)
-	runs_directory.mkdir(parents=True, exist_ok=True)
-	with open(runs_directory / (run_id+'.json'), 'w') as file:
-		wrapped_results = {
-			"run_id": run_id,
-			"test_results": test_results
-		}
-		json.dump(wrapped_results, file, indent=1)
-
-	# Identify the most recent runs (by filename ordering)
-	run_files = glob.glob(str(runs_directory)+'/*')
-	run_files.sort(reverse=True)
-	run_files = run_files[0:10]
-	runs = []
-	for run_file in run_files:
-		with open(run_file) as file:
-			runs.append(json.load(file))
-	# Index the runs
-	runs_index = {}
-	for run in runs:
-		runs_index[run["run_id"]] = run
-	# Identify the superset of test_ids (across runs)
-	test_ids_set = set()
-	for run in runs: 
-		for test_result in run["test_results"]:
-			test_ids_set.add(test_result["test_id"])
-	test_ids = list(test_ids_set).sort()
-
-	report_markdown_lines = []
-	report_markdown_lines.append('# Status Report')
-	report_markdown_lines.append('')
-
-	header_segments = []
-	header_break_segments = []
-	for run in runs:
-		header_segments.append(run['run_id'])
-		header_break_segments.append('--')
-	report_markdown_lines.append('| Test ID | Title | ' + ' | '.join(header_segments) + ' |')
-	report_markdown_lines.append('|---------|-------|' + '|'.join(header_break_segments) + '|')
-
-	print('\n'.join(report_markdown_lines))
-
-
-def _validated_and_sanitize_test_results_json(test_results):
-	for test_result in test_results:
-		for field in ['title', 'test_id', 'description', 'state', 'logs']:
-			_ensure_required_test_field(test_result, field)
-		for field in ['test_id']:
-			_sanitize_test_field(test_result, field)
-	
-def _ensure_required_test_field(test_result, field_name):
-	if field_name not in test_result or test_result[field_name] == '':
-		raise Exception('Invalid/missing test field "'+field_name+'"')
-
-def _sanitize_test_field(test_result, field_name):
-	# Prevent any special chars (safe for filesystem)
-	test_result[field_name] = _invalid_char_pattern.sub('_', test_result[field_name])
-	# Prevent "..", which can walk out of directories (safe for filesystem)
-	test_result[field_name] = _multi_dot_pattern.sub('_', test_result[field_name])
-
 def _sanitize_filesystem_path(path_str):
 	# Prevent any special chars (safe for filesystem)
 	path_str = _unsafe_filesystem_chars.sub('_', path_str)
 	return path_str
+
+
 
 
 # TODO: Remove debug
@@ -302,4 +303,15 @@ report_test_run(this_script_dir / 'out',
 ya ya
 last line""")
 
-generate_markdown(this_script_dir / 'out', timezone('US/Eastern'))
+out_dir = this_script_dir / 'out'
+define_config(out_dir,
+	config_timezone='US/Pacific',
+	config_summary_title="My Cool System",
+	config_overview_section_md="""Overview
+--------
+
+This is a mock overview section.
+
+Status
+--------""")
+generate_markdown(out_dir)
